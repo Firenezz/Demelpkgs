59f16bd1557492b5755c9e1827a8d90bbd3c4883
Demelary <9827644+Firenezz@users.noreply.github.com>
2024-05-10 15:42:48 -0400

    Add cargo to the flake

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -3,27 +3,43 @@
     naersk.url = "github:nix-community/naersk/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
 
-    systems.url = "path:./flake.systems.nix" # github:nix-systems/default
+    systems-default = { url = "path:./flake.systems.nix"; flake = false; }; # github:nix-systems/default
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-          naersk-lib = pkgs.callPackage naersk { };
-        in
-        {
-          formatter = pkgs.nixpkgs-fmt;
-          packages = {
-            default = naersk-lib.buildPackage { src = ./.; };
+  outputs = { self, nixpkgs, utils, naersk, flake-parts, systems-default, ... } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = import systems-default;
+
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: let 
+        manifest = (builtins.fromTOML (builtins.readFile ./Cargo.toml));
+        
+        overlay = final: prev: {
+          "${manifest.package.name}" = final.callPackage ./. { inherit naersk; };
+        };
+
+        overlay-pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              self.overlay
+            ];
           };
-          devShells = {
-            default = with pkgs; mkShell {
-              buildInputs = [ git cargo rustc rustfmt pre-commit rustPackages.clippy ];
-              RUST_SRC_PATH = rustPlatform.rustLibSrc;
-            };
-          };
-        });
+      in {
+
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            self.overlay
+          ];
+        };
+
+        packages."${manifest.package.name}" = pkgs."${manifest.package.name}";
+      };
+    };
+    
 }
